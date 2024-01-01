ad957f427dd6fe4af2f0690487fb5ebb28791741
Jen-Chieh Shen <jcs090218@gmail.com>
2024-01-01 01:40:08 -0800

    chore: Update copyright year

--- com-css-sort.el
+++ com-css-sort.el
@@ -1,6 +1,6 @@
 ;;; com-css-sort.el --- Common way of sorting the CSS attributes  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2018  Shen, Jen-Chieh
+;; Copyright (C) 2018-2024  Shen, Jen-Chieh
 ;; Created date 2018-04-30 14:26:37
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
